dbd9f096d9bade72cfba67ffcfb9c88cfbcf6a99
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-08-02 22:51:26 -0500

    test: crear clase TestAtributos.java para probar los constructores de la clase Maquina.java

---
+++ src/com/krakedev/artesanal/test/TestAtributos.java
@@ -0,0 +1,12 @@
+package com.krakedev.artesanal.test;
+import com.krakedev.artesanal.Maquina;
+
+public class TestAtributos {
+	public static void main(String[] args) {
+		Maquina rubia = new Maquina("Pilsener", "Cerveza rubia", 0.02, 10000);
+		rubia.imprimir();
+		rubia.setNombreCerveza("Golden Ale");
+		rubia.setDescripcion("Cerveza con aroma mas intenso");
+		rubia.imprimir();
+	}
+}
