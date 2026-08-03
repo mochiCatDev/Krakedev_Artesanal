c51fc2920167a86cbf0ecc5519994e95e0867c5a
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-08-03 08:52:15 -0500

    test: crear TestServir.java para probar el método servirCerveza de la clase Maquina.java

---
+++ src/com/krakedev/artesanal/test/TestServir.java
@@ -0,0 +1,31 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.Maquina;
+
+public class TestServir {
+	public static void main(String[] args) {
+		Maquina rubia = new Maquina("Pilsener", "Cerveza fría", 0.02, 8000);
+		
+		System.out.println("-> Estado Inicial");
+		rubia.imprimir();
+		System.out.println("-> Llenando Máquina...");
+		rubia.llenarMaquina();
+		rubia.imprimir();
+		
+		System.out.println("-> Servir 1000Ml...");
+		double valor;
+		valor = rubia.servirCerveza(1000);
+		System.out.println("Valor a pagar: " + valor + "USD");
+		rubia.imprimir();
+		
+		System.out.println("-> Servir 2000Ml...");
+		valor = rubia.servirCerveza(2000);
+		System.out.println("Valor a pagar: " + valor + "USD");
+		rubia.imprimir();
+		
+		System.out.println("-> Servir 5000Ml...");
+		valor = rubia.servirCerveza(5000);
+		System.out.println("Valor a pagar: " + valor + "USD");
+		rubia.imprimir();
+	}
+}
