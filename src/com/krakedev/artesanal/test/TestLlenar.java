7d16086901acea56b2a61dbb0c9afa30c4063934
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-08-02 23:06:56 -0500

    test: crear clase TestLlenar.java para probar el método llenarMaquina de la clase Maquina.java

---
+++ src/com/krakedev/artesanal/test/TestLlenar.java
@@ -0,0 +1,22 @@
+package com.krakedev.artesanal.test;
+import com.krakedev.artesanal.Maquina;
+
+public class TestLlenar {
+	public static void main(String[] args) {
+		Maquina rubia = new Maquina("Pilsener", "Cerveza fría", 0.02, 80000);
+		System.out.println("========= MAQUINA RUBIA =========");
+		System.out.println("-> Estado Por Defecto");
+		rubia.imprimir();
+		System.out.println("-> Llenar Maquina");
+		rubia.llenarMaquina();
+		rubia.imprimir();
+		
+		Maquina negra = new Maquina("Club", "Cerveza buena", 0.03);
+		System.out.println("========= MAQUINA NEGRA =========");
+		System.out.println("-> Estado Por Defecto");
+		negra.imprimir();
+		System.out.println("-> Llenar Maquina");
+		negra.llenarMaquina();
+		negra.imprimir();
+	}
+}
