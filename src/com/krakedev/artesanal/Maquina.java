35c19802d125c4fab47b6ad984b08b1385e511e0
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-08-02 19:55:27 -0500

    feat: añadir método imprimir en la clase Maquina.java
    El método imprime en consola todos los atributos de la clase.

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -6,34 +6,49 @@ public class Maquina {
 	private double precioPorMl;
 	private double capacidadMaxima;
 	private double cantidadActual;
-	
+
 	// === GETTERS ===
-	
+
 	public String getNombreCerveza() {
 		return nombreCerveza;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public double getPrecioPorMl() {
 		return precioPorMl;
 	}
+
 	public double getCapacidadMaxima() {
 		return capacidadMaxima;
 	}
+
 	public double getCantidadActual() {
 		return cantidadActual;
 	}
-	
+
 	// === SETTERS ===
-	
+
 	public void setNombreCerveza(String nombreCerveza) {
 		this.nombreCerveza = nombreCerveza;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public void setPrecioPorMl(double precioPorMl) {
 		this.precioPorMl = precioPorMl;
+	}
+
+	// === IMPRIMIR ===
+
+	public void imprimir() {
+		String mensaje;
+		mensaje = "Nombre Cerveza: " + nombreCerveza + ", Descripción: " + descripcion + ", Precio por ML: "
+				+ precioPorMl + ", Capacidad Máxima: " + capacidadMaxima + ", Cantidad Actual: " + cantidadActual;
+		System.out.println(mensaje);
 	}
 }
