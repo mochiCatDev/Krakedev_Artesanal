131c1044db1b848f2b0c3db0d49e17bfd4d3bb70
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-08-06 16:03:33 -0500

    test(TestMaquina): pasar parametros al instanciar un negocio.

--- src/com/krakedev/artesanal/testNegocio/TestNegocio.java
+++ src/com/krakedev/artesanal/testNegocio/TestNegocio.java
@@ -1,12 +1,18 @@
 package com.krakedev.artesanal.testNegocio;
 
+import com.krakedev.artesanal.Maquina;
 import com.krakedev.artesanal.Negocio;
 
 public class TestNegocio {
 	public static void main(String[] args) {
-		Negocio negocio1 = new Negocio();
+		Maquina maquina1 = new Maquina("016", "Cerveza Club", "Cerveza Fría", 0.02, 8000);
+		Negocio negocio1 = new Negocio("Mi Negocio", maquina1);
 		
 		System.out.println("Nombre: " + negocio1.getNombre());
 		System.out.println("Maquina: " + negocio1.getMaquinaA());
+		
+		Maquina m1 = negocio1.getMaquinaA();
+		double capacidad = m1.getCapacidadMaxima();
+		System.out.println("Capacidad Maxima: " + capacidad);
 	}
 }
